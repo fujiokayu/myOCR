a13a7023a349afc39b845e66dd98da12dbd0c7af
fujiokayu <fujiokayu@gmail.com>
2020-09-27 16:58:57 +0900

    remove unnecessary code

--- app/index.js
+++ app/index.js
@@ -12,40 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-'use strict';
+"use strict"
 
 // [START functions_ocr_setup]
-// Get a reference to the Pub/Sub component
-const {PubSub} = require('@google-cloud/pubsub');
-const pubsub = new PubSub();
 // Get a reference to the Cloud Storage component
-const {Storage} = require('@google-cloud/storage');
-const storage = new Storage();
+const { Storage } = require("@google-cloud/storage")
+const storage = new Storage()
 
 // Get a reference to the Cloud Vision API component
-const Vision = require('@google-cloud/vision');
-const vision = new Vision.ImageAnnotatorClient();
-
-// Get a reference to the Translate API component
-const {Translate} = require('@google-cloud/translate').v2;
-const translate = new Translate();
-
+const Vision = require("@google-cloud/vision")
+const vision = new Vision.ImageAnnotatorClient()
 // [END functions_ocr_setup]
-
-// [START functions_ocr_publish]
-/**
- * Publishes the result to the given pubsub topic and returns a Promise.
- *
- * @param {string} topicName Name of the topic on which to publish.
- * @param {object} data The message data to publish.
- */
-const publishResult = async (topicName, data) => {
-  const dataBuffer = Buffer.from(JSON.stringify(data));
-
-  const [topic] = await pubsub.topic(topicName).get({autoCreate: true});
-  topic.publish(dataBuffer);
-};
-// [END functions_ocr_publish]
 
 // [START functions_ocr_detect]
 /**
@@ -56,38 +33,16 @@ const publishResult = async (topicName, data) => {
  * @returns {Promise}
  */
 const detectText = async (bucketName, filename) => {
-  console.log(`Looking for text in image ${filename}`);
+  console.log(`Looking for text in image ${filename}`)
   const [textDetections] = await vision.textDetection(
     `gs://${bucketName}/${filename}`
-  );
-  const [annotation] = textDetections.textAnnotations;
-  const text = annotation ? annotation.description : '';
-  console.log(`Extracted text from image:`, text);
+  )
+  const [annotation] = textDetections.textAnnotations
+  const text = annotation ? annotation.description : ""
+  console.log(`Extracted text from image:`, text)
 
-  let [translateDetection] = await translate.detect(text);
-  if (Array.isArray(translateDetection)) {
-    [translateDetection] = translateDetection;
-  }
-  console.log(
-    `Detected language "${translateDetection.language}" for ${filename}`
-  );
-
-  // Submit a message to the bus for each language we're going to translate to
-  const TO_LANGS = process.env.TO_LANG.split(',');
-  const topicName = process.env.TRANSLATE_TOPIC;
-
-  const tasks = TO_LANGS.map((lang) => {
-    const messageData = {
-      text: text,
-      filename: filename,
-      lang: lang,
-    };
-
-    return publishResult(topicName, messageData);
-  });
-
-  return Promise.all(tasks);
-};
+  return Promise.all(tasks)
+}
 // [END functions_ocr_detect]
 
 // [START functions_ocr_rename]
@@ -99,8 +54,8 @@ const detectText = async (bucketName, filename) => {
  * @returns {string} The new filename.
  */
 const renameImageForSave = (filename, lang) => {
-  return `${filename}_to_${lang}.txt`;
-};
+  return `${filename}_to_${lang}.txt`
+}
 // [END functions_ocr_rename]
 
 // [START functions_ocr_process]
@@ -112,71 +67,23 @@ const renameImageForSave = (filename, lang) => {
  * @param {object} event A Google Cloud Storage File object.
  */
 exports.processImage = async (event) => {
-  const {bucket, name} = event;
+  const { bucket, name } = event
 
   if (!bucket) {
     throw new Error(
       'Bucket not provided. Make sure you have a "bucket" property in your request'
-    );
+    )
   }
   if (!name) {
     throw new Error(
       'Filename not provided. Make sure you have a "name" property in your request'
-    );
+    )
   }
 
-  await detectText(bucket, name);
-  console.log(`File ${name} processed.`);
-};
+  await detectText(bucket, name)
+  console.log(`File ${name} processed.`)
+}
 // [END functions_ocr_process]
-
-// [START functions_ocr_translate]
-/**
- * This function is exported by index.js, and is executed when
- * a message is published to the Cloud Pub/Sub topic specified
- * by the TRANSLATE_TOPIC environment variable. The function
- * translates text using the Google Translate API.
- *
- * @param {object} event The Cloud Pub/Sub Message object.
- * @param {string} {messageObject}.data The "data" property of the Cloud Pub/Sub
- * Message. This property will be a base64-encoded string that you must decode.
- */
-exports.translateText = async (event) => {
-  const pubsubData = event.data;
-  const jsonStr = Buffer.from(pubsubData, 'base64').toString();
-  const {text, filename, lang} = JSON.parse(jsonStr);
-
-  if (!text) {
-    throw new Error(
-      'Text not provided. Make sure you have a "text" property in your request'
-    );
-  }
-  if (!filename) {
-    throw new Error(
-      'Filename not provided. Make sure you have a "filename" property in your request'
-    );
-  }
-  if (!lang) {
-    throw new Error(
-      'Language not provided. Make sure you have a "lang" property in your request'
-    );
-  }
-
-  console.log(`Translating text into ${lang}`);
-  const [translation] = await translate.translate(text, lang);
-
-  console.log(`Translated text:`, translation);
-
-  const messageData = {
-    text: translation,
-    filename: filename,
-    lang: lang,
-  };
-
-  await publishResult(process.env.RESULT_TOPIC, messageData);
-  console.log(`Text translated to ${lang}`);
-};
-// [END functions_ocr_translate]
 
 // [START functions_ocr_save]
 /**
@@ -190,35 +97,35 @@ exports.translateText = async (event) => {
  * Message. This property will be a base64-encoded string that you must decode.
  */
 exports.saveResult = async (event) => {
-  const pubsubData = event.data;
-  const jsonStr = Buffer.from(pubsubData, 'base64').toString();
-  const {text, filename, lang} = JSON.parse(jsonStr);
+  const pubsubData = event.data
+  const jsonStr = Buffer.from(pubsubData, "base64").toString()
+  const { text, filename, lang } = JSON.parse(jsonStr)
 
   if (!text) {
     throw new Error(
       'Text not provided. Make sure you have a "text" property in your request'
-    );
+    )
   }
   if (!filename) {
     throw new Error(
       'Filename not provided. Make sure you have a "filename" property in your request'
-    );
+    )
   }
   if (!lang) {
     throw new Error(
       'Language not provided. Make sure you have a "lang" property in your request'
-    );
+    )
   }
 
-  console.log(`Received request to save file ${filename}`);
+  console.log(`Received request to save file ${filename}`)
 
-  const bucketName = process.env.RESULT_BUCKET;
-  const newFilename = renameImageForSave(filename, lang);
-  const file = storage.bucket(bucketName).file(newFilename);
+  const bucketName = process.env.RESULT_BUCKET
+  const newFilename = renameImageForSave(filename, lang)
+  const file = storage.bucket(bucketName).file(newFilename)
 
-  console.log(`Saving result to ${newFilename} in bucket ${bucketName}`);
+  console.log(`Saving result to ${newFilename} in bucket ${bucketName}`)
 
-  await file.save(text);
-  console.log(`File saved.`);
-};
+  await file.save(text)
+  console.log(`File saved.`)
+}
 // [END functions_ocr_save]
