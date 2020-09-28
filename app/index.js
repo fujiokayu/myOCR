0389a36386c935a0162e4a32ea8207fd1afa948b
fujiokayu <fujiokayu@gmail.com>
2020-09-28 21:18:40 +0900

    remove unnecessary code

--- app/index.js
+++ app/index.js
@@ -43,19 +43,6 @@ const detectText = async (bucketName, filename) => {
 }
 // [END functions_ocr_detect]
 
-// [START functions_ocr_rename]
-/**
- * Appends a .txt suffix to the image name.
- *
- * @param {string} filename Name of a file.
- * @param {string} lang Language to append.
- * @returns {string} The new filename.
- */
-const renameImageForSave = (filename, lang) => {
-  return `${filename}_to_${lang}.txt`
-}
-// [END functions_ocr_rename]
-
 // [START functions_ocr_process]
 /**
  * This function is exported by index.js, and is executed when
@@ -82,43 +69,3 @@ exports.processImage = async (event) => {
   console.log(`File ${name} processed.`)
 }
 // [END functions_ocr_process]
-
-// [START functions_ocr_save]
-/**
- * This function is exported by index.js, and is executed when
- * a message is published to the Cloud Pub/Sub topic specified
- * by the RESULT_TOPIC environment variable. The function saves
- * the data packet to a file in GCS.
- *
- * @param {object} event The Cloud Pub/Sub Message object.
- * @param {string} {messageObject}.data The "data" property of the Cloud Pub/Sub
- * Message. This property will be a base64-encoded string that you must decode.
- */
-exports.saveResult = async (event) => {
-  const pubsubData = event.data
-  const jsonStr = Buffer.from(pubsubData, "base64").toString()
-  const { text, filename } = JSON.parse(jsonStr)
-
-  if (!text) {
-    throw new Error(
-      'Text not provided. Make sure you have a "text" property in your request'
-    )
-  }
-  if (!filename) {
-    throw new Error(
-      'Filename not provided. Make sure you have a "filename" property in your request'
-    )
-  }
-
-  console.log(`Received request to save file ${filename}`)
-
-  const bucketName = process.env.RESULT_BUCKET
-  const newFilename = renameImageForSave(filename, lang)
-  const file = storage.bucket(bucketName).file(newFilename)
-
-  console.log(`Saving result to ${newFilename} in bucket ${bucketName}`)
-
-  await file.save(text)
-  console.log(`File saved.`)
-}
-// [END functions_ocr_save]
